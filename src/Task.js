add7218440d924fe4397fda0e975eb4bb31fec49
NephDev <vakih35@gmail.com>
2024-10-26 00:35:50 +0700

    Gallery-App

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Muhammad Furqon Rizqi" // TODO: replace this
+const studentId = "FS11844837" // TODO: replace this
+
+export { url, studentName, studentId }
